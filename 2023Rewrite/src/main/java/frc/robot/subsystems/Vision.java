ba5fd8150ece2dec68cd7c5595b1f0be58ee3086
Elijah Sargeant <39015548+ElijahSargeant@users.noreply.github.com>
2023-04-24 18:38:21 -0400

    bruh

--- 2023Rewrite/src/main/java/frc/robot/subsystems/Vision.java
+++ 2023Rewrite/src/main/java/frc/robot/subsystems/Vision.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.networktables.DoubleArraySubscriber;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
